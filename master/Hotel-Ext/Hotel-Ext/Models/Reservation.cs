3a6dbc6ea40ce5d1664c11559dcf7340b51b1527
Putri Irma Kusmayati Bratawijaya <putriirmak@yahoo.com>
2015-11-03 14:01:28 +0700

    add total amount into reservation

--- master/Hotel-Ext/Hotel-Ext/Models/Reservation.cs
+++ master/Hotel-Ext/Hotel-Ext/Models/Reservation.cs
@@ -14,13 +14,15 @@ namespace Hotel.Models
         [Display(Name = "Reservation Number ")]
         public string ReservationNo { get; set; }
         [Display(Name = "Reserve Date")]
-        public DateTime ReservationStartDate { get; set; }
+        public System.DateTime ReservationStartDate { get; set; }
         [Display(Name = "Until")]
-        public DateTime ReservationEndDate { get; set; }
+        public System.DateTime ReservationEndDate { get; set; }
         [Display(Name = "Check In")]
-        public DateTime CheckInTime { get; set; }
+        public System.DateTime CheckInTime { get; set; }
         [Display(Name = "Check Out")]
-        public DateTime CheckOutTime { get; set; }
+        public System.DateTime CheckOutTime { get; set; }
+        [Display(Name = "Total Amount")]
+        public Decimal totalAmount { get; set; }
         [Display(Name = "Check Out")]
         public int? UserHotelId { get; set; }
 
